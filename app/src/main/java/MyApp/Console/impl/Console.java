e87312dde5ee02614739c404bc3e88a33f7ce34f
Naaaggi <naguigh23@gmail.com>
2021-11-21 17:59:08 +0100

    first commit

---
+++ app/src/main/java/MyApp/Console/impl/Console.java
@@ -0,0 +1,38 @@
+package MyApp.Console.impl;
+
+import MyApp.Console.IConsole;
+import java.util.Scanner;
+
+public class Console implements IConsole {
+	final private Scanner scanner = new Scanner(System.in);
+
+	@Override
+	public String readString(String text) {
+		System.out.print(text);
+		String input = scanner.nextLine();
+		return input.replaceAll("[^a-zA-Z0-9]", " ");
+	}
+
+	@Override
+	public int readInteger(String text) {
+		System.out.print(text);
+		int result = 0;
+		while (true) {
+			String s = scanner.nextLine();
+			try {
+				result = Integer.parseInt(s);
+			} catch (NumberFormatException e) {
+				System.out.print(text);
+				continue;
+			}
+			break;
+		}
+		return result;
+	}
+
+	@Override
+	public void write(String s) {
+		System.out.println(s);
+	}
+
+}
